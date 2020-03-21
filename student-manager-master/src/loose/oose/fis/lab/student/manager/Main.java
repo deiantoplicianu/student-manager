a9fcb0cfbf8a2ffa4c7638f474a3a191c4d421b8
Deian <deian.toplicianu@gamil.com>
2020-03-21 20:32:41 +0200

    ChangeCont

--- student-manager-master/src/loose/oose/fis/lab/student/manager/Main.java
+++ student-manager-master/src/loose/oose/fis/lab/student/manager/Main.java
@@ -9,25 +9,24 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-<<<<<<< HEAD
+//<<<<<<< HEAD
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
         primaryStage.setTitle("FIS - Student Manager");
         primaryStage.setScene(new Scene(root, 600, 400));
-=======
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//=======
+    /*public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
->>>>>>> ffe86ea4918f8c44a8387714aaa27165e8eb5e14
+        primaryStage.setScene(new Scene(root, 300, 275));*/
+//>>>>>>> ffe86ea4918f8c44a8387714aaa27165e8eb5e14
         primaryStage.show();
     }
 
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 
-=======
->>>>>>> ffe86ea4918f8c44a8387714aaa27165e8eb5e14
+//=======
+//>>>>>>> ffe86ea4918f8c44a8387714aaa27165e8eb5e14
     public static void main(String[] args) {
         launch(args);
     }
